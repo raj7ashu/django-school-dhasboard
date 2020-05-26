95a83db71cd2f5e362c274b8b6ebb94c34baf37b
Rajesh Kumar <raj7ashu@gmail.com>
2020-05-26 19:19:21 +0530

    Hellow world with django app

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -22,6 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '9^!_6+caim_@fle$91r=5vrqd*hh*5inli4at9!df_i9*aj&3@'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'dashboard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
